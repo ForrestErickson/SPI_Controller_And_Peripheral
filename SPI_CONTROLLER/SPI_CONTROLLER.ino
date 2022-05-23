eb3ed9d8f8914c31e88dd18e63f0b3aa9575cde6
Forrest Erickson <forresterickson@comcast.net>
2022-05-23 08:18:12 -0400

    Change to Controller/Peripheral termonology.

--- SPI_CONTROLLER/SPI_CONTROLLER.ino
+++ SPI_CONTROLLER/SPI_CONTROLLER.ino
@@ -24,33 +24,33 @@ void setup (void)
   pinMode(LED, OUTPUT);                   //Sets pin 7 as Output
   SPI.begin();                            //Begins the SPI commnuication
   SPI.setClockDivider(SPI_CLOCK_DIV8);    //Sets clock for SPI communication at 8 (16/8=2Mhz)
-  digitalWrite(SS, HIGH);                 // Setting SlaveSelect as HIGH (So master doesnt connnect with slave)
+  digitalWrite(SS, HIGH);                 // Setting PeripheralSelect as HIGH (So controller doesnt connnect with peripheral)
 }// end setup()
 
 
 void loop(void)
 {
-  byte Mastersend, Mastereceive;
+  byte Controllersend, Controllereceive;
   buttonvalue = digitalRead(ipbutton);   //Reads the status of the pin 2
-  if (buttonvalue == HIGH)               //Logic for Setting x value (To be sent to slave) depending upon input from pin 2
+  if (buttonvalue == HIGH)               //Logic for Setting x value (To be sent to peripheral) depending upon input from pin 2
   {
     x = 1;
   }else
   {
     x = 0;
   }
-  digitalWrite(SS, LOW);                  //Starts communication with Slave connected to master
-  Mastersend = x;
-  Mastereceive = SPI.transfer(Mastersend); //Send the mastersend value to slave also receives value from slave
-  if (Mastereceive == 1)                  //Logic for setting the LED output depending upon value received from slave
+  digitalWrite(SS, LOW);                  //Starts communication with Peripheral connected to controller
+  Controllersend = x;
+  Controllereceive = SPI.transfer(Controllersend); //Send the controllersend value to peripheral also receives value from peripheral
+  if (Controllereceive == 1)                  //Logic for setting the LED output depending upon value received from peripheral
   {
     digitalWrite(LED, HIGH);             //Sets pin 7 HIGH
-    Serial.println("Master LED ON");
+    Serial.println("Controller LED ON");
   }
   else
   {
     digitalWrite(LED, LOW);              //Sets pin 7 LOW
-    Serial.println("Master LED OFF");
+    Serial.println("Controller LED OFF");
   }
   delay(1000);
 }// end loop()
--- SPI_PERIPHERAL/SPI_PERIPHERAL.ino
+++ SPI_PERIPHERAL/SPI_PERIPHERAL.ino
@@ -6,7 +6,7 @@
 */
 
 
-//SPI PHERIPHERAL (ARDUINO UNO)
+//SPI PERIPHERAL (ARDUINO UNO)
 //SPI COMMUNICATION BETWEEN TWO ARDUINO UNOs
 //CIRCUIT DIGEST
 
@@ -16,7 +16,7 @@
 #define buttonpin 2
 
 volatile boolean received;
-volatile byte Slavereceived,Slavesend;
+volatile byte Peripheralreceived,Peripheralsend;
 int buttonvalue;
 int x;
 
@@ -25,8 +25,8 @@ void setup()
   Serial.begin(115200);
   pinMode(buttonpin,INPUT);               // Setting pin 2 as INPUT
   pinMode(LEDpin,OUTPUT);                 // Setting pin 7 as OUTPUT
-  pinMode(MISO,OUTPUT);                   //Sets MISO as OUTPUT (Have to Send data to Master IN 
-  SPCR |= _BV(SPE);                       //Turn on SPI in Slave Mode
+  pinMode(MISO,OUTPUT);                   //Sets MISO as OUTPUT (Have to Send data to Controller IN 
+  SPCR |= _BV(SPE);                       //Turn on SPI in Peripheral Mode
   received = false;
   SPI.attachInterrupt();                  //Interuupt ON is set for SPI commnucation
 }//end setup()
@@ -34,33 +34,33 @@ void setup()
 
 ISR (SPI_STC_vect)                        //Inerrrput routine function 
 {
-  Slavereceived = SPDR;         // Value received from master if store in variable slavereceived
+  Peripheralreceived = SPDR;         // Value received from controller if store in variable Peripheralreceived
   received = true;                        //Sets received as True 
 }//end ISR
 
 
 void loop()
-{ if(received)                            //Logic to SET LED ON OR OFF depending upon the value recerived from master
+{ if(received)                            //Logic to SET LED ON OR OFF depending upon the value recerived from controller
    {
-      if (Slavereceived==1) 
+      if (Peripheralreceived==1) 
       {
         digitalWrite(LEDpin,HIGH);         //Sets pin 7 as HIGH LED ON
-        Serial.println("Slave LED ON");
+        Serial.println("Peripheral LED ON");
       }else
       {
         digitalWrite(LEDpin,LOW);          //Sets pin 7 as LOW LED OFF
-        Serial.println("Slave LED OFF");
+        Serial.println("Peripheral LED OFF");
       }
       buttonvalue = digitalRead(buttonpin);  // Reads the status of the pin 2
-      if (buttonvalue == HIGH)               //Logic to set the value of x to send to master
+      if (buttonvalue == HIGH)               //Logic to set the value of x to send to controller
       {
         x=1;
       }else
       {
         x=0;
       }
-  Slavesend=x;                             
-  SPDR = Slavesend;                           //Sends the x value to master via SPDR 
+  Peripheralsend=x;                             
+  SPDR = Peripheralsend;                           //Sends the x value to controller via SPDR 
 
   delay(1000);
 
