8d641306baa85443ce1b68cf6b590b70231d8b2a
Forrest Erickson <forresterickson@comcast.net>
2022-05-24 09:36:58 -0400

    Very dirty commit but working both directions.

--- SPI_CONTROLLER/SPI_CONTROLLER.ino
+++ SPI_CONTROLLER/SPI_CONTROLLER.ino
@@ -10,48 +10,79 @@
 //SPI COMMUNICATION BETWEEN TWO ARDUINO UNOs
 //CIRCUIT DIGEST
 
+/* Hardware Notes
+   SPI Line Pin in Arduino
+  MOSI 11 or ICSP-4
+  MISO 12 or ICSP-1
+  SCK 13 or ICSP-3
+  SS 10
+*/
 
 #include<SPI.h>                             //Library for SPI 
 #define LED_PIN 7                               //Add this LED + Resistor
-#define BUTTON_PIN 2                          //Button to +5V, 10K Resistor to GND
+//#define BUTTON_PIN 2                          //Button to +5V, 10K Resistor to GND
+#define BUTTON_PIN 2                          //Button to GND, 10K Resistor to +5V.
 
 int buttonValue;
 int x;
+byte controllerSend, controlleReceive;
+
 
 void setup (void)
 {
   Serial.begin(115200);                   //Starts Serial Communication at Baud Rate 115200
+  delay(500);
+  Serial.println("Starting SPI Controller.");
   pinMode(BUTTON_PIN, INPUT);               //Sets pin 2 as input
   pinMode(LED_PIN, OUTPUT);                   //Sets pin 7 as Output
+
   SPI.begin();                            //Begins the SPI commnuication
   SPI.setClockDivider(SPI_CLOCK_DIV8);    //Sets clock for SPI communication at 8 (16/8=2Mhz)
+  pinMode(SS, OUTPUT);                 // Set nCS for output
+  pinMode(MISO, INPUT);                  //Sets MISO as INPUT for testing contention ONLY.
+
   digitalWrite(SS, HIGH);                 // Setting PeripheralSelect as HIGH (So controller doesnt connnect with peripheral)
+  delay(100);
 }// end setup()
 
 
 void loop(void)
 {
-  byte controllerSend, controlleReceive;
-  buttonValue = digitalRead(BUTTON_PIN);   //Reads the status of the pin 2
-  if (buttonValue == HIGH)               //Logic for Setting x value (To be sent to peripheral) depending upon input from pin 2
-  {
-    x = 1;
-  }else
-  {
-    x = 0;
-  }
+
+  //  buttonValue = digitalRead(BUTTON_PIN);   //Reads the status of the pin 2
+  //  if (buttonValue == HIGH)               //Logic for Setting x value (To be sent to peripheral) depending upon input from pin 2
+  //    //  if (buttonValue == LOW)               //Logic for Setting x value (To be sent to peripheral) depending upon input from pin 2
+  //  {
+  //    x = 1;
+  //  }
+  //  else
+  //  {
+  //    x = 0;
+  //  }
+
+  //  x = buttonValue;
+  controllerSend = digitalRead(BUTTON_PIN);
+  Serial.print("SPI Controler: Button Status: ");
+  Serial.print(controllerSend);
+
   digitalWrite(SS, LOW);                  //Starts communication with Peripheral connected to controller
-  controllerSend = x;
   controlleReceive = SPI.transfer(controllerSend); //Send the controllersend value to peripheral also receives value from peripheral
+  digitalWrite(SS, HIGH);                  //End communication with Peripheral connected to controller
+
+  Serial.print(", Peripherial: ");
+  Serial.println(controlleReceive);
+
+  //  Serial.print("Peripherial LED: ");
   if (controlleReceive == 1)                  //Logic for setting the LED output depending upon value received from peripheral
   {
     digitalWrite(LED_PIN, HIGH);             //Sets pin 7 HIGH
-    Serial.println("Controller LED ON");
+    //  Serial.println(" ON");
   }
   else
   {
     digitalWrite(LED_PIN, LOW);              //Sets pin 7 LOW
-    Serial.println("Controller LED OFF");
+    //Serial.println(" OFF");
   }
-  delay(1000);
+
+  delay(100);
 }// end loop()
--- SPI_PERIPHERAL/SPI_PERIPHERAL.ino
+++ SPI_PERIPHERAL/SPI_PERIPHERAL.ino
@@ -11,59 +11,88 @@
 //SPI COMMUNICATION BETWEEN TWO ARDUINO UNOs
 //CIRCUIT DIGEST
 
+/* Hardware Notes
+   SPI Line Pin in Arduino
+  MOSI 11 or ICSP-4
+  MISO 12 or ICSP-1
+  SCK 13 or ICSP-3
+  SS 10
+*/
+
 //Pramoth.T
 #include<SPI.h>
 #define LED_PIN 7
-#define BUTTON_PIN 2
+//#define BUTTON_PIN 2                          //Button to +5V, 10K Resistor to GND
+#define BUTTON_PIN 2                          //Button to GND, 10K Resistor to +5V.
 
 volatile boolean isReceived_SPI;
-volatile byte peripheralReceived,peripheralSend;
+volatile byte peripheralReceived, peripheralSend;
 int buttonValue;
 int x;
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(BUTTON_PIN,INPUT);               // Setting pin 2 as INPUT
-  pinMode(LED_PIN,OUTPUT);                 // Setting pin 7 as OUTPUT
-  pinMode(MISO,OUTPUT);                   //Sets MISO as OUTPUT (Have to Send data to Controller IN 
+  delay(500);
+  Serial.println("Starting SPI Peripheral.");
+
+  pinMode(BUTTON_PIN, INPUT);              // Setting pin 2 as INPUT
+  pinMode(LED_PIN, OUTPUT);                // Setting pin 7 as OUTPUT
+
+  //  SPI.begin();    // IMPORTANT. Do not set SPI.begin for a peripherial device.
+  pinMode(SS, INPUT_PULLUP);                    //Sets SS as input for peripherial
+  pinMode(MOSI, OUTPUT);    //ToDo, this seams backwards.
+//  pinMode(MISO, INPUT);    //ToDo, this seams backwards.
+  pinMode(MISO, OUTPUT);    //try this.
+  pinMode(SCK, INPUT);                  //Sets clock as input
   SPCR |= _BV(SPE);                       //Turn on SPI in Peripheral Mode
   isReceived_SPI = false;
   SPI.attachInterrupt();                  //Interuupt ON is set for SPI commnucation
 }//end setup()
 
 
-ISR (SPI_STC_vect)                        //Inerrrput routine function 
+ISR (SPI_STC_vect)                        //Inerrrput routine function
 {
   peripheralReceived = SPDR;         // Value received from controller if store in variable peripheralReceived
-  isReceived_SPI = true;                        //Sets isReceived_SPI as True 
+  isReceived_SPI = true;                        //Sets isReceived_SPI as True
+//  SPDR = peripheralSend;  
 }//end ISR
+
+void updateFromSPI() {
+  if (isReceived_SPI)                           //Logic to SET LED ON OR OFF depending upon the value recerived from controller
+  {
+        if (peripheralReceived == 1)
+        {
+          digitalWrite(LED_PIN, HIGH);        //Sets pin 7 as HIGH LED ON
+//          Serial.println("Peripheral LED ON");
+        } else
+        {
+          digitalWrite(LED_PIN, LOW);         //Sets pin 7 as LOW LED OFF
+//          Serial.println("Peripheral LED OFF");
+        }
+    buttonValue = digitalRead(BUTTON_PIN);  // Reads the status of the pin 2
+    if (buttonValue == HIGH)               //Logic to set the value of x to send to controller
+    {
+      x = 1;
+    }
+    else
+    {
+      x = 0;
+    }
+    peripheralSend = x;
+    //    Serial.print("Peripherial Switch ");
+    //    Serial.println(x);
+    SPDR = peripheralSend;    //SPDR register of data to be shiffted out.
+
+    //    delay(1000);
+    Serial.println("SPI_PERIPHERAL, isReceived_SPI True.");
+  }// end received
+
+}//end updateFromSPI
 
 
 void loop()
-{ if(isReceived_SPI)                            //Logic to SET LED ON OR OFF depending upon the value recerived from controller
-   {
-      if (peripheralReceived==1) 
-      {
-        digitalWrite(LED_PIN,HIGH);         //Sets pin 7 as HIGH LED ON
-        Serial.println("Peripheral LED ON");
-      }else
-      {
-        digitalWrite(LED_PIN,LOW);          //Sets pin 7 as LOW LED OFF
-        Serial.println("Peripheral LED OFF");
-      }
-      buttonValue = digitalRead(BUTTON_PIN);  // Reads the status of the pin 2
-      if (buttonValue == HIGH)               //Logic to set the value of x to send to controller
-      {
-        x=1;
-      }else
-      {
-        x=0;
-      }
-  peripheralSend=x;                             
-  SPDR = peripheralSend;                           //Sends the x value to controller via SPDR 
+{
 
-  delay(1000);
-
-}// end received
+  updateFromSPI();
 }//end loop()
